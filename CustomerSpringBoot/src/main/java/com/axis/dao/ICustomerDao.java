ecfae5b78632946410d62cd74fca8f5dc809ab3e
singhReetesh31 <94595371+singhReetesh31@users.noreply.github.com>
2021-12-06 22:00:55 +0530

    Delete CustomerSpringBoot directory

--- CustomerSpringBoot/src/main/java/com/axis/dao/ICustomerDao.java
+++
@@ -1,9 +0,0 @@
-package com.axis.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.axis.entity.Customer;
-
-public interface ICustomerDao extends JpaRepository<Customer, Integer>{ 
-
-}
